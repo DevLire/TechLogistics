0b5e458867b958cdc4001203feed77655e3f6561
Baruc Alejandria <alejandriacbaruc@gmail.com>
2026-06-02 02:06:32 -0500

    Add cache control headers to auth check request

--- app/src/core/actions/auth/check-auth.action.ts
+++ app/src/core/actions/auth/check-auth.action.ts
@@ -8,6 +8,12 @@ export const checkAuthAction = async (
     const { data } = await api.get<AuthResponse>('/auth/check-status', {
       headers: {
         'x-device-id': deviceId || '',
+        'Cache-Control': 'no-cache, no-store, must-revalidate',
+        Pragma: 'no-cache',
+        Expires: '0',
+      },
+      params: {
+        _t: new Date().getTime(),
       },
     });
     return data;
