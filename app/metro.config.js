a1df964adaa091857b342989ccec4749a18c54bc
Baruc Alejandria <alejandriacbaruc@gmail.com>
2026-08-09 20:15:22 -0500

    feat(shared): update package configuration and add shared dependency

--- app/metro.config.js
+++ app/metro.config.js
@@ -1,7 +1,10 @@
+const path = require('path');
 const { getDefaultConfig } = require('expo/metro-config');
 const { withNativewind } = require('nativewind/metro');
 
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
+
+config.watchFolders = [path.resolve(__dirname, '../packages/shared')];
 
 module.exports = withNativewind(config);
